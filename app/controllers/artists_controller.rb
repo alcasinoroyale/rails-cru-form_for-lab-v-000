947391a10de2d5d2a4e78f3ab29f88cdf1330ad7
Alex Zdatny <alexzdatny@gmail.com>
2018-09-13 22:17:05 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,7 +5,7 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    @artist = Artist.new(params.require(:artist).permit(:name))
+    @artist = Artist.new(artist_params)
     @artist.save
     redirect_to artist_path(@artist)
   end
